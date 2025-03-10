959cae0dbf14464320f9dc2b82bc883507cef81c
mattriley619 <76889891+mattriley619@users.noreply.github.com>
2025-03-10 17:08:08 -0400

    Add files via upload

--- Project3.py
+++ Project3.py
@@ -1,6 +1,6 @@
-#------------------------------------
-#Problem 1
-#------------------------------------
+print("------------------------------------------")
+print("Problem 1")
+print("------------------------------------------")
 
 fruit = ["**apple**", "!g!r!a!p!e!", "egnaro", "yyyrrrrrreeehhhccc"]
 
@@ -13,9 +13,9 @@ slicedFruit.append(fruit[3][::-3])
 
 print(slicedFruit)
 
-#------------------------------------
-#Problem 2
-#------------------------------------
+print("------------------------------------------")
+print("Problem 2")
+print("------------------------------------------")
 
 name = "Lee Harvey Oswald"
 split = name.split()
@@ -31,9 +31,9 @@ print(username)
 print(password)
 
 
-#------------------------------------
-#Problem 3
-#------------------------------------
+print("------------------------------------------")
+print("Problem 3")
+print("------------------------------------------")
 
 patrick = \
 {
@@ -49,9 +49,19 @@ patrick = \
     'number' : 'A1376047',
     'class' : '5'
 }
+print(f' {"_" *61}')
+print(f'| ------------------ {"Bikini Bottom".upper(): ^20} --------------------|')
+print(f'|{"Driver License".upper(): >37} {"CLASS: " + patrick["class"]: >22} |')
+print(f'|{"EXPIRES: " + patrick["expiration"]: <} {patrick["number"]: ^30} {"|": >13}')
+print(f'|___________________ {"|": >42}')
+print(f'|        W          |{patrick["name"]: ^20} {"|": >21}')
+print(f'|       / \\         |{patrick["street"]: ^21} {"|": >20}')
+print(f'|      /   \\        |{patrick["city"]: ^21} {"|": >20}')
+print(f'|     / *  *\\       | {"|": >41}')
+print(f'|    /  (__) \\      |{"SEX: " + patrick["sex"][0]: >10} {"HAIR: " + patrick["hair"].upper(): >14} {"EYES: " + patrick["eyes"].upper(): >14} |')
+print(f'|   /         \\     |{"HT: " + patrick["height"]: >12} {"WT: " + patrick["weight"]: >10} {"|": >18}')
+print(f' {"-" *61}')
 
-print(f'----------Bikini Bottom----------')
-print(f'--------Driver\'s License----------')
 
 
 
